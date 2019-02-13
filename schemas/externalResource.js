1bf0bdaa7b87c13fa959eb02540978ab5fc839f7
uf0 <scnepa222@gmail.com>
2019-02-13 12:27:15 +0100

    added block resources

--- schemas/externalResource.js
+++ schemas/externalResource.js
@@ -1,23 +1,43 @@
 export default {
-  name: 'externalresource',
-  title: 'Link Resource',
-  type: 'document',
-  fields: [{
-      name: 'name',
-      title: 'Name',
-      type: 'string'
-    }, {
-      name: 'category',
-      title: 'Category',
-      type: 'reference',
-      to: [{
-        type: 'resourcecategory'
-      }]
+  name: "externalresource",
+  title: "Link Resource",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      title: "Name",
+      type: "blockContentResources"
     },
     {
-      title: 'Link URL',
-      name: 'linkUrl',
-      type: 'url'
+      name: "category",
+      title: "Category",
+      type: "reference",
+      to: [
+        {
+          type: "resourcecategory"
+        }
+      ]
+    },
+    {
+      title: "Link URL",
+      name: "linkUrl",
+      type: "url"
     }
-  ]
-}
+  ],
+  preview: {
+    select: {
+      name: "name"
+    },
+    prepare(value) {
+      const block = (value.name || []).find(block => block._type === "block");
+      return {
+        title: block
+          ? block.children
+              .filter(child => child._type === "span")
+              .map(span => span.text)
+              .join("")
+          : "No title"
+      };
+    }
+  }
+};
--- schemas/internalResource.js
+++ schemas/internalResource.js
@@ -1,28 +1,48 @@
 export default {
-  name: 'internalresource',
-  title: 'Document Resource',
-  type: 'document',
-  fields: [{
-      name: 'name',
-      title: 'Name',
-      type: 'string'
-    }, {
-      name: 'category',
-      title: 'Category',
-      type: 'reference',
-      to: [{
-        type: 'resourcecategory'
-      }]
+  name: "internalresource",
+  title: "Document Resource",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      title: "Name",
+      type: "blockContentResources"
     },
     {
-      title: 'Document',
-      name: 'document',
-      type: 'file'
+      name: "category",
+      title: "Category",
+      type: "reference",
+      to: [
+        {
+          type: "resourcecategory"
+        }
+      ]
     },
     {
-      name: 'private',
-      title: 'Private Document',
-      type: 'boolean'
+      title: "Document",
+      name: "document",
+      type: "file"
+    },
+    {
+      name: "private",
+      title: "Private Document",
+      type: "boolean"
     }
-  ]
-}
+  ],
+  preview: {
+    select: {
+      name: "name"
+    },
+    prepare(value) {
+      const block = (value.name || []).find(block => block._type === "block");
+      return {
+        title: block
+          ? block.children
+              .filter(child => child._type === "span")
+              .map(span => span.text)
+              .join("")
+          : "No title"
+      };
+    }
+  }
+};
